6ec020b27aeab64e1171efe565d16e0141ce8d00
Diego Figueroa <diego.castillo@student.kit.edu>
2024-06-19 15:28:28 +0200

    add function to create common constellations

--- code/help_functions.py
+++ code/help_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 from torch.nn.functional import conv1d
 
@@ -9,3 +11,27 @@ def cascade_filters(filt_1, filt_2):
 def convolve(signal, filt):
     filt = torch.resolve_conj(torch.flip(filt, [-1]))
     return conv1d(signal, filt, padding='same')
+
+def common_constellation(mod, M):
+    if mod == "PAM":
+        constellation = np.linspace(0, 1, num=M, endpoint=True)
+    elif mod == "ASK":
+        constellation = np.linspace(-1, 1, num=M, endpoint=True)
+    elif mod == "SQAM":
+        X_base = np.array([1, 1j, -1, -1j])
+        constellation = np.array([], dtype=np.complex64)
+        for ii in range(M // len(X_base)):
+            constellation = np.append(constellation, (ii + 1) * X_base)
+    elif mod == "QAM":
+        Mp = int(np.sqrt(M))
+        constellation1D = np.linspace(-1, 1, num=Mp, endpoint=True)
+        constellation = np.reshape(np.add.outer(constellation1D.T, 1j * constellation1D), -1)
+    elif mod == "DDQAM":
+        angle = np.arccos(1/3)
+        constellation = np.kron(np.arange(1, M//4+1),np.exp(np.array([0,angle,np.pi,np.pi+angle])*1j))
+    else:
+        ValueError("mod should be PAM, ASK, SQAM, QAM or DDQAM")
+
+    constellation = constellation / np.sqrt(np.mean(np.abs(constellation) ** 2))
+    return torch.tensor(constellation)
+
