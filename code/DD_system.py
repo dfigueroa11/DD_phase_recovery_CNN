6d28d12d7699a211f494ebf4d25dbd19745f7c33
Diego Figueroa <diego.castillo@student.kit.edu>
2024-06-19 17:21:27 +0200

    add a function to perform easily common setups of the system

--- code/DD_system.py
+++ code/DD_system.py
@@ -6,13 +6,16 @@ import help_functions as hlp
 
 class DD_system():
 
-    def __init__(self, N_os, N_sim, constellation, diff_encoder, pulse_shape, ch_imp_resp, rx_filt, ):
+    def __init__(self, N_os, N_sim, constellation, diff_encoder, pulse_shape, ch_imp_resp, rx_filt):
         self.N_os = N_os
         self.N_sim = N_sim  
         self.d = N_sim//N_os 
         self.constellation = constellation
         self.diff_encoder = diff_encoder
-        self.tx_filt = hlp.cascade_filters(pulse_shape, ch_imp_resp).view(1,1,-1)
+        if pulse_shape is not None and ch_imp_resp is not None:
+            self.tx_filt = hlp.cascade_filters(pulse_shape, ch_imp_resp).view(1,1,-1)
+        else:
+            self.tx_filt = None
         self.rx_filt = rx_filt.view(1,1,-1)
 
     def simulate_transmission(self, batch_size, N_sym, Ptx):
@@ -28,19 +31,47 @@ class DD_system():
         y = hlp.convolve(y, self.rx_filt)
         return u, x, y[:,:,self.d-1::self.d]
         
+def set_up_DD_system(N_os, N_sim, **kwargs):
+    if {"mod_format", "M"} <= kwargs.keys():
+        constellation = hlp.common_constellation(kwargs["mod_format"], kwargs["M"])
+    elif "constellation" in kwargs.keys():
+        constellation = kwargs["constellation"]
+    else:
+        constellation = None
+    if {"alpha", "N_taps"} <= kwargs.keys():
+        pulse_shape = hlp.rcos_filt(kwargs["alpha"], kwargs["N_taps"], N_sim, 1)
+    elif "pulse_shape" in kwargs.keys():
+        pulse_shape = kwargs["pulse_shape"]
+    else:
+        pulse_shape = None
+    if {"L_link", "R_sym", "beta2", "N_taps"} <= kwargs.keys():
+        ch_imp_resp = hlp.chrom_disp_filt(kwargs["L_link"], kwargs["R_sym"], kwargs["beta2"], kwargs["N_taps"], N_sim)
+    elif "ch_imp_resp" in kwargs.keys():
+        ch_imp_resp = kwargs["ch_imp_resp"]
+    else:
+        ch_imp_resp = None
+    if "diff_encoder" in kwargs.keys():
+        diff_encoder = kwargs["diff_encoder"]
+    else:
+        diff_encoder = None
+    if "rx_filt" in kwargs.keys():
+        rx_filt = kwargs["rx_filt"]
+    elif N_sim > N_os:
+        rx_filt = hlp.rcos_filt(0, len(pulse_shape), N_os, 1, dtype=torch.float32)
+    else:
+        rx_filt = torch.tensor([1.])
+    return DD_system(N_os, N_sim, constellation , diff_encoder, pulse_shape, ch_imp_resp, rx_filt)
+
 
 
 
 
 if __name__ == "__main__":
 
-    constellation = torch.tensor([1 + 1j, 1 - 1j, -1 + 1j, -1 -1j])
-    pulse_shape = torch.tensor([0, 0, 0, 1, 0, 0, 0])
-    ch_imp_resp = torch.tensor([0, 0, 0, 1, 0, 0, 0])
-    rx_filt = torch.tensor([1.])
-    N_os = 5
-    N_sim = 10
-
-    system = DD_system(N_os, N_sim, constellation , None, pulse_shape, ch_imp_resp, rx_filt)
-    u, x = system.simulate_transmission(3,20,torch.tensor([10000]))
+    system = set_up_DD_system(N_os= 2, N_sim=2,
+                              mod_format="PAM", M=4,
+                              N_taps=7,     # for all
+                              alpha=0.2, 
+                              L_link=10e3, R_sym=20e9, beta2=-2e26)
+    u, x, y = system.simulate_transmission(3,20,torch.tensor([10000]))
     
