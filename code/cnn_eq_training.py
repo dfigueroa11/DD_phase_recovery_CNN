352696b86ff8c3e938676e930aac81d8abefcd8e
Diego Figueroa <diego.castillo@student.kit.edu>
2024-06-26 14:28:42 +0200

    CNN ready to train and try different approaches

--- code/cnn_eq_training.py
+++ code/cnn_eq_training.py
@@ -1,6 +1,8 @@
 import torch
 import torch.optim as optim
 from torch.nn import MSELoss 
+import matplotlib.pyplot as plt
+import numpy as np
 
 import help_functions as hlp
 import DD_system
@@ -12,9 +14,9 @@ print("We are using the following device for learning:",device)
 
 dd_system = DD_system.set_up_DD_system(N_os= 2, N_sim=2,
                             mod_format="PAM", M=4,
-                            N_taps=11,     # for all
+                            N_taps=11,     
                             alpha=0, 
-                            L_link=10e3, R_sym=20e9, beta2=-2e26)
+                            L_link=0e3, R_sym=20e9, beta2=-2e26)
 
 
 num_ch = [1,3,3,2]
@@ -27,23 +29,40 @@ cnn_equalizer = CNN_equalizer.CNN_equalizer(num_ch, ker_lens, strides, paddings,
 optimizer = optim.Adam(cnn_equalizer.parameters(), eps=1e-07)
 
 
+batches_per_epoch = 1
+batch_size_per_epoch = [1,]
+N_sym = 2000
+SNR_dB_steps = torch.tensor([*range(20,21)])
+loss_func = MSELoss(reduction='mean')
 
-batches_per_epoch = 30
-batch_size_per_epoch = [100, 400]
-Ptx_dB_steps = torch.tensor([*range(20,21)])
-N_sym = 200
 
+loss_evolution = []
 cnn_equalizer.train()
-loss_func = MSELoss()
-for Ptx_dB in Ptx_dB_steps:
+for SNR_dB in SNR_dB_steps:
     for batch_size in batch_size_per_epoch:
         for i in range(batches_per_epoch):
-            u, x, y = dd_system.simulate_transmission(batch_size, N_sym, Ptx_dB)
+            u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
             x_hat = cnn_equalizer(y)
-
+            
             x_real_imag = torch.cat((torch.real(x),torch.imag(x)), dim=1)
             loss = loss_func(x_real_imag,x_hat)
-
+            
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+            loss_evolution.append(loss.detach().cpu().numpy())
+
+
+
+u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
+cnn_equalizer.eval()
+x_hat = cnn_equalizer(y)
+
+x = x.detach().cpu().numpy()
+x_hat = x_hat.detach().cpu().numpy()
+
+plt.figure()
+plt.scatter(np.real(x)**2,np.imag(x), alpha=0.5)
+plt.scatter(y[:,:,1::2],np.zeros_like(y[:,:,1::2]), alpha=0.5)
+plt.scatter(x_hat[:,0,:],x_hat[:,1,:], alpha=0.5)
+plt.show()
