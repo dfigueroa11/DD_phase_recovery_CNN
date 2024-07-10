ca7cccfd2bbb007e258277c2bab2be76c77c51d1
Diego Figueroa <diego.castillo@student.kit.edu>
2024-07-10 16:31:36 +0200

    separate the trainings in MSE between symbols and CNN output, and between ideal DD (1 ISI) and CNN output

---
+++ code/cnn_eq_training_idealDD.py
@@ -0,0 +1,95 @@
+import torch
+import torch.optim as optim
+from torch.nn import MSELoss 
+import matplotlib.pyplot as plt
+import numpy as np
+
+import help_functions as hlp
+import DD_system
+import CNN_equalizer
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("We are using the following device for learning:",device)
+
+N_taps = 41
+dd_system = hlp.set_up_DD_system(N_os= 2, N_sim=2,
+                            mod_format="ASK", M=4, sqrt_flag=True,
+                            diff_encoder=False,
+                            N_taps=N_taps,     
+                            alpha=0, 
+                            L_link=30e3, R_sym=35e9, beta2=-2.168e-26)
+
+### to determine an appropriate number of taps for the tx_filter choose a big number of taps above
+### and use the code below to determine an appropriate number of taps
+# energy_criteria = 99
+# filt, N_taps = hlp.filt_windowing(torch.squeeze(dd_system.tx_filt), energy_criteria)
+# print(f"{N_taps} tap are needed to contain the {energy_criteria}% of the energy")
+# plt.figure()
+# t = np.arange(-np.floor(torch.numel(filt)/2),np.floor(torch.numel(filt)/2)+1)
+# plt.stem(t, np.abs(filt)**2)
+# t = np.arange(-np.floor(torch.numel(dd_system.tx_filt)/2),np.floor(torch.numel(dd_system.tx_filt)/2)+1)
+# plt.stem(t, np.abs(torch.squeeze(dd_system.tx_filt))**2, linefmt=':')
+# plt.show()
+
+num_ch = [1,3,3,1]
+ker_lens = [21,15,9]
+strides = [1,1,1]
+activ_func = torch.nn.ELU()
+cnn_equalizer = CNN_equalizer.CNN_equalizer(num_ch, ker_lens, strides, activ_func)
+
+optimizer = optim.Adam(cnn_equalizer.parameters(), eps=1e-07)
+
+
+batches_per_epoch = 300
+batch_size_per_epoch = [100, 300, 500]
+N_sym = 1000
+SNR_dB_steps = [*range(40,41)]
+loss_func = MSELoss(reduction='mean')
+
+checkpoint_per_epoch = 10
+loss_evolution = [-1]
+cnn_equalizer.train()
+for SNR_dB in SNR_dB_steps:
+    print(f'train model for SNR {SNR_dB} dB')
+    for batch_size in batch_size_per_epoch:
+        for i in range(batches_per_epoch):
+            u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
+            y_hat = cnn_equalizer(y)
+            
+            y_1_ISI = hlp.DD_1sym_ISI(x,dd_system.tx_filt[0,0,N_taps//2],dd_system.tx_filt[0,0,N_taps//2+1])*dd_system.rx_filt[0,0,0]
+            loss = loss_func(y_1_ISI, y_hat)
+            
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            loss_evolution.append(loss.detach().cpu().numpy())
+            if (i+1)%(batches_per_epoch//checkpoint_per_epoch) == 0:  
+                print(f"\tBatch size {batch_size:_}, Train step {i:_} from {batches_per_epoch:_}, {loss_evolution[-1]}")
+            
+
+
+u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
+cnn_equalizer.eval()
+y_hat = cnn_equalizer(y)
+
+y_1_ISI = hlp.DD_1sym_ISI(x,dd_system.tx_filt[0,0,N_taps//2],dd_system.tx_filt[0,0,N_taps//2+1])*dd_system.rx_filt[0,0,0]
+y_1_ISI = y_1_ISI.detach().cpu().numpy()
+y_hat = y_hat.detach().cpu().numpy()
+
+
+plt.figure()
+plt.title("Phase sample")
+plt.hist(y_1_ISI[:,:,0::2].flatten(), 20, alpha=1, label='ideal DD')
+plt.hist(y[:,:,0::2].flatten(), 200, alpha=0.5, label='DD out')
+plt.hist(y_hat[:,:,0::2].flatten(), 200, alpha=0.5, label='CNN out')
+plt.ylim(0,2e4)
+plt.legend(loc='upper right')
+
+plt.figure()
+plt.title("Magnitude sample")
+plt.hist(y_1_ISI[:,:,1::2].flatten(), 20, alpha=1, label='ideal DD',)
+plt.hist(y[:,:,1::2].flatten(), 200, alpha=0.5, label='DD out')
+plt.hist(y_hat[:,:,1::2].flatten(), 200, alpha=0.5, label='CNN out')
+plt.ylim(0,2e4)
+plt.legend(loc='upper right')
+plt.show()
--- code/cnn_eq_training_sym.py
+++ code/cnn_eq_training_sym.py
@@ -13,22 +13,23 @@ print("We are using the following device for learning:",device)
 
 
 dd_system = hlp.set_up_DD_system(N_os= 2, N_sim=2,
-                            mod_format="PAM", M=16, sqrt_flag=True,
+                            mod_format="PAM", M=4, sqrt_flag=False,
                             diff_encoder=False,
-                            N_taps=43,     
-                            alpha=1, 
-                            L_link=0e3, R_sym=35e9, beta2=-2.168e-26)
+                            N_taps=41,     
+                            alpha=0, 
+                            L_link=30e3, R_sym=35e9, beta2=-2.168e-26)
 
 # ### to determine an appropriate number of taps for the tx_filter choose a big number of taps above
 # ### and use the code below to determine an appropriate number of taps
-# filt, N_taps = hlp.filt_windowing(torch.squeeze(dd_system.tx_filt), energy_criteria=99)
-# print(N_taps)
+# energy_criteria = 98
+# filt, N_taps = hlp.filt_windowing(torch.squeeze(dd_system.tx_filt), energy_criteria)
+# print(f"{N_taps} tap are needed to contain the {energy_criteria}% of the energy")
 # plt.figure()
 # plt.stem(torch.abs(filt)**2)
 # plt.show()
 
 num_ch = [1,3,3,2]
-ker_lens = [11,9,9]
+ker_lens = [21,15,9]
 strides = [1,1,2]
 activ_func = torch.nn.ELU()
 cnn_equalizer = CNN_equalizer.CNN_equalizer(num_ch, ker_lens, strides, activ_func)
@@ -36,16 +37,17 @@ cnn_equalizer = CNN_equalizer.CNN_equalizer(num_ch, ker_lens, strides, activ_fun
 optimizer = optim.Adam(cnn_equalizer.parameters(), eps=1e-07)
 
 
-batches_per_epoch = 1
-batch_size_per_epoch = [1,]
+batches_per_epoch = 300
+batch_size_per_epoch = [100, 300, 500]
 N_sym = 1000
-SNR_dB_steps = [*range(400,401)]
+SNR_dB_steps = [*range(40,41)]
 loss_func = MSELoss(reduction='mean')
 
-
+checkpoint_per_epoch = 10
 loss_evolution = []
 cnn_equalizer.train()
 for SNR_dB in SNR_dB_steps:
+    print(f'train model for SNR {SNR_dB} dB')
     for batch_size in batch_size_per_epoch:
         for i in range(batches_per_epoch):
             u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
@@ -58,7 +60,9 @@ for SNR_dB in SNR_dB_steps:
             optimizer.step()
             optimizer.zero_grad()
             loss_evolution.append(loss.detach().cpu().numpy())
-
+            if (i+1)%(batches_per_epoch//checkpoint_per_epoch) == 0:  
+                print(f"\tBatch size {batch_size:_}, Train step {i:_} from {batches_per_epoch:_}, {loss_evolution[-1]}")
+            
 
 
 u, x, y = dd_system.simulate_transmission(batch_size, N_sym, SNR_dB)
@@ -69,8 +73,7 @@ x = x.detach().cpu().numpy()
 x_hat = x_hat.detach().cpu().numpy()
 
 plt.figure()
-# plt.scatter(x_hat[:,0,:],x_hat[:,1,:], alpha=0.5, label="CNN_eq")
-plt.scatter(y[:,:,1::2],np.zeros_like(y[:,:,1::2]), alpha=0.5, label="DD_output")
-# plt.scatter(np.real(x),np.imag(x), alpha=0.5, label="DD_input")
+plt.scatter(x_hat[:,0,:],x_hat[:,1,:], alpha=0.5, label="CNN_eq")
+plt.scatter(np.real(x),np.imag(x), alpha=0.5, label="DD_input")
 plt.legend()
 plt.show()
