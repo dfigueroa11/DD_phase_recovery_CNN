b656fb4e6484ba4dad89d7c51fcf6b04be494510
Diego Figueroa <diego.castillo@student.kit.edu>
2024-08-20 17:23:35 +0200

    big test

--- code/test_CNN.sh
+++ code/test_CNN.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+for i in {1..4}; do
+echo "Running iteration $i"
 #####################################
 python3.11 cnn_eq_training_phase_diff.py "ASK" 2
 python3.11 cnn_eq_training_phase_diff.py "ASK" 4
@@ -22,3 +25,7 @@ python3.11 cnn_eq_training_odd_samp.py "ASK" 4
 
 #####################################
 python3.11 cnn_eq_training_odd_samp_phase_in.py "ASK" 4
+
+done
+
+echo "All iterations completed."
