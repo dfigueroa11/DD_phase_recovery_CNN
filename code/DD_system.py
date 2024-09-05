30289218c6ea9fe059c125819e554760935a4e81
Diego Figueroa <diego.castillo@student.kit.edu>
2024-09-05 16:34:53 +0200

    add properties multi_mag_const and multi_phase_const

--- code/DD_system.py
+++ code/DD_system.py
@@ -29,6 +29,8 @@ class DD_system():
         self.N_sim = N_sim
         self.d = N_sim//N_os
         self.constellation = constellation.to(device)
+        self.multi_mag_const = torch.unique(torch.round(torch.abs(self.constellation), decimals=5)).numel() > 1
+        self.multi_phase_const = torch.unique(torch.round(torch.angle(self.constellation), decimals=5)).numel() > 1
         self.diff_encoder = diff_encoder
         if pulse_shape is not None and ch_imp_resp is not None:
             self.tx_filt = hlp.norm_filt(N_sim, hlp.cascade_filters(pulse_shape, ch_imp_resp)).view(1,1,-1).to(device)
@@ -49,7 +51,7 @@ class DD_system():
         x:  the symbols after the differential encoding (Tensor of size (batch_size, 1, N_sym)) if diff_encoder is None u = x  
         y:  the samples after the transmission (Tensor of size (batch_size, 1, N_sym*N_os))
         '''
-        Ptx_lin = 10**(Ptx_dB/10)
+        Ptx_lin = torch.tensor([10**(Ptx_dB/10)], device=self.device, dtype=torch.float32)
         u = torch.sqrt(Ptx_lin)*self.constellation[torch.randint(torch.numel(self.constellation),[batch_size, 1, N_sym])]
         if self.diff_encoder is not None:
             x = self.diff_encoder.encode(u)
