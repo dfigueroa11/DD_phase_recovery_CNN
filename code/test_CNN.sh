20f1da5cf8b097eaaf2e4a271682f4fe642e3422
Diego Figueroa <diego.castillo@student.kit.edu>
2024-09-10 09:22:04 +0200

    fix arguments in .sh

--- code/test_CNN.sh
+++ code/test_CNN.sh
@@ -3,11 +3,11 @@
 for i in {1..5}; do
 echo "Running iteration $i"
 #####################################
-nice -n 5 python3.11 cnn_eq_training_sym.py "ASK" 2
-nice -n 5 python3.11 cnn_eq_training_sym.py "ASK" 4
-nice -n 5 python3.11 cnn_eq_training_sym.py "PAM" 2
-nice -n 5 python3.11 cnn_eq_training_sym.py "PAM" 4
-nice -n 5 python3.11 cnn_eq_training_sym.py "QAM" 4
+nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 2
+nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 4
+nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 2
+nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 4
+nice -n 5 python3.11 cnn_eq_training_sym.py -m QAM -o 4
 
 done
 
