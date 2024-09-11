a46ece63e75e9e00af2dec06a8a83431ff128585
Diego Figueroa <diego.castillo@student.kit.edu>
2024-09-11 16:37:02 +0200

    fix typo error, and start code for max min and mean performance

--- results_analysis/analyze_multiple_trains.py
+++ results_analysis/analyze_multiple_trains.py
@@ -16,23 +16,18 @@ def create_results_folder(path,n_copy):
 def read_all_data(num_folders):
     all_data = []
     for i in range(num_folders):
-        with open(f'{path}/{folder}_{i}/SER_results.txt', 'r') as file:
-            lines = file.readlines()
-        data = []
-        for line in lines:
-            # Extract all numerical values including decimals and scientific notation
-            numbers = re.findall(r'[-+]?\d*\.\d+e[-+]?\d+|[-+]?\d*\.\d+|\d+', line)
-            # Convert extracted numbers to float and append to data
-            data.append([float(num) for num in numbers])
-        all_data.append(data)
-    return all_data
+        all_data.append(np.loadtxt(f'{path}/{folder}_{i}/SER_results.txt', delimiter=",", skiprows=1))
+    return np.array(all_data)
 
-def pick_best_snr_data(num_folders):
+def pick_best_ser_data(num_folders):
     all_data = read_all_data(num_folders)
-    all_data = np.array(all_data)
     return np.argmin(all_data[:,:,-1], axis=0), np.min(all_data, axis=0)
 
-def write_snr_txt(data, path):
+def pick_max_min_mean_ser_data(num_folders):
+    all_data = read_all_data(num_folders)
+    return np.argmin(all_data[:,:,-1], axis=0), np.min(all_data, axis=0)
+
+def write_ser_txt(data, path):
     with open(f"{path}/SER_results.txt", 'w') as file:
         for data_line in data:
             file.write(f"lr={data_line[0]}, L_link={data_line[1]:.0f}km, alpha={data_line[2]}, SNR={data_line[3]}dB --> SER:{data_line[4]:.10e}\n")
@@ -49,28 +44,15 @@ def save_best_images(data, idx, result_path, folder):
             pass
 
 
-path = "/Users/diegofigueroa/Desktop/results"
+path = "/Users/diegofigueroa/Desktop/"
 path_best = f"{path}_best"
 
-folders = ["DDQAM8_phase_diff_mag_in",
-           "DDQAM4_phase_diff",
-           "PAM4_odd_samp",
-           "ASK4_odd_samp",
-           "QAM4_phase_diff",
-           "ASK4_phase",
-           "ASK4_odd_samp_phase_in",
-           "QAM4_phase",
-           "ASK4_phase_diff_mag_in",
-           "PAM2_odd_samp",
-           "ASK4_phase_diff",
-           "ASK2_phase",
-           "DDQAM4_phase",
-           "ASK2_phase_diff"]
+folders = ["PAM2_sym"]
 
 all_data = []
 
 for folder in folders:
-    best_idx, best_snr_data = pick_best_snr_data(5)
+    best_idx, best_ser_data = pick_best_ser_data(5)
     result_path = create_results_folder(f"{path_best}/{folder}",0)
-    write_snr_txt(best_snr_data, result_path)
-    save_best_images(best_snr_data, best_idx, result_path, folder)
+    write_ser_txt(best_ser_data, result_path)
+    save_best_images(best_ser_data, best_idx, result_path, folder)
