934e0e94fd312f699fb92bb67402da6b7b822c2b
Diego Figueroa <diego.castillo@student.kit.edu>
2024-09-11 19:30:48 +0200

    analysis of data and writing of the .txt done

--- results_analysis/analyze_multiple_trains.py
+++ results_analysis/analyze_multiple_trains.py
@@ -19,18 +19,31 @@ def read_all_data(num_folders):
         all_data.append(np.loadtxt(f'{path}/{folder}_{i}/SER_results.txt', delimiter=",", skiprows=1))
     return np.array(all_data)
 
-def pick_best_ser_data(num_folders):
-    all_data = read_all_data(num_folders)
-    return np.argmin(all_data[:,:,-1], axis=0), np.min(all_data, axis=0)
+def find_replace_non_convergence(all_data, threshold):
+    for j in range(4, all_data.shape[-1]):
+        all_data[:,:,j] = np.where(all_data[:,:,-1]<threshold, np.nan, all_data[:,:,j])
+    return all_data
 
-def pick_max_min_mean_ser_data(num_folders):
-    all_data = read_all_data(num_folders)
-    return np.argmin(all_data[:,:,-1], axis=0), np.min(all_data, axis=0)
+def pick_max_min_mean_data(all_data):
+    min_data, min_idx = np.nanmin(all_data, axis=0), np.nanargmin(all_data,axis=0)
+    max_data, max_idx = np.nanmax(all_data, axis=0), np.nanargmax(all_data,axis=0)
+    mean_data = np.nanmean(all_data, axis=0)
+    return np.stack(np.stack([min_data, mean_data, max_data])), np.stack([min_idx, max_idx])
 
-def write_ser_txt(data, path):
-    with open(f"{path}/SER_results.txt", 'w') as file:
-        for data_line in data:
-            file.write(f"lr={data_line[0]}, L_link={data_line[1]:.0f}km, alpha={data_line[2]}, SNR={data_line[3]}dB --> SER:{data_line[4]:.10e}\n")
+def find_convergence_rate(all_data):
+    return 1-np.mean(np.isnan(all_data[:,:,-1]),axis=0)
+
+def write_ser_txt(data, conv_rate, path_source, path_destination):
+    with open(f'{path_source}/SER_results.txt', 'r') as source_file:
+        first_line = source_file.readline()
+    with open(f"{path_destination}/SER_results.txt", 'w') as file:
+        file.write(first_line+",conv_rate")
+        for i in range(data.shape[1]):
+            line = f"{data[0,i,0]},{data[0,i,1]},{data[0,i,2]},{data[0,i,3]}"
+            for j in range(4, data.shape[-1]):
+                line += f",[{data[0,i,j]:.10e},{data[1,i,j]:.10e},{data[2,i,j]:.10e}]"
+            line += f",{conv_rate[i]}"
+            file.write(f"{line}\n")
 
 def save_best_images(data, idx, result_path, folder):
     for i, data_line in zip(idx, data):
@@ -44,7 +57,7 @@ def save_best_images(data, idx, result_path, folder):
             pass
 
 
-path = "/Users/diegofigueroa/Desktop/"
+path = "/Users/diegofigueroa/Desktop/results"
 path_best = f"{path}_best"
 
 folders = ["PAM2_sym"]
@@ -52,7 +65,10 @@ folders = ["PAM2_sym"]
 all_data = []
 
 for folder in folders:
-    best_idx, best_ser_data = pick_best_ser_data(5)
+    all_data = read_all_data(num_folders=5)
+    all_data = find_replace_non_convergence(all_data, threshold=1e-4)
+    data, idx = pick_max_min_mean_data(all_data)
+    conv_rate = find_convergence_rate(all_data)
     result_path = create_results_folder(f"{path_best}/{folder}",0)
-    write_ser_txt(best_ser_data, result_path)
-    save_best_images(best_ser_data, best_idx, result_path, folder)
+    write_ser_txt(data, conv_rate, f"{path}/{folder}_{0}", result_path)
+    # # save_best_images(best_ser_data, best_idx, result_path, folder)
