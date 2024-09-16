addb408db158900d03c4094c4cbe1a5fbfac9f17
Diego Figueroa <diego.castillo@student.kit.edu>
2024-09-16 11:51:17 +0200

    back to noise manage as in plabst: noise power = 1 and vary P_tx

--- code/DD_system.py
+++ code/DD_system.py
@@ -38,28 +38,28 @@ class DD_system():
             self.tx_filt = None
         self.rx_filt = hlp.norm_filt(N_sim, rx_filt).view(1,1,-1).to(device)
 
-    def simulate_transmission(self, batch_size, N_sym, SNR_dB):
-        ''' Simulates the transmission of B batches and N symbols per batch with the given SNR
+    def simulate_transmission(self, batch_size, N_sym, Ptx_dB):
+        ''' Simulates the transmission of B batches and N symbols per batch with the given Ptx
 
         Arguments:
         batch_size:     number of batches to simulate (integer)
         N_sym:          number of symbols per batch (integer)
-        SNR_dB:         SNR in dB used for the simulation (float)
+        Ptx_dB:         transmission power in dB used for the simulation (float)
         
         Returns:
         u:  the symbols before the differential encoding (Tensor of size (batch_size, 1, N_sym))
         x:  the symbols after the differential encoding (Tensor of size (batch_size, 1, N_sym)) if diff_encoder is None u = x  
         y:  the samples after the transmission (Tensor of size (batch_size, 1, N_sym*N_os))
         '''
-        u = self.constellation[torch.randint(torch.numel(self.constellation),[batch_size, 1, N_sym])]
+        Ptx_lin = torch.tensor([10**(Ptx_dB/10)], device=self.device, dtype=torch.float32)
+        u = torch.sqrt(Ptx_lin)*self.constellation[torch.randint(torch.numel(self.constellation),[batch_size, 1, N_sym])]
         if self.diff_encoder is not None:
             x = self.diff_encoder.encode(u)
         else:
             x = u
         x_up = torch.kron(x,torch.eye(self.N_sim, device=self.device)[-1])
         z = torch.square(torch.abs(hlp.convolve(x_up, self.tx_filt)))
-        SNR_lin = torch.tensor([10**(SNR_dB/10)], device=self.device, dtype=torch.float32)
-        var_n = torch.tensor([self.N_sim/(2*SNR_lin)], dtype=torch.float32, device=self.device)
+        var_n = torch.tensor([self.N_sim/2], dtype=torch.float32, device=self.device)
         y = z + torch.sqrt(var_n)*torch.randn_like(z)
         y = hlp.convolve(y, self.rx_filt)
         return u, x, y[:,:,self.d-1::self.d]
