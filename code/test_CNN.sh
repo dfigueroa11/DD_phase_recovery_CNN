c4b087a03b312dbae1d395b57909a9b3b1084e2c
Diego Figueroa <diego.castillo@student.kit.edu>
2024-10-23 10:01:36 +0200

    test for the big CNN

--- code/test_CNN.sh
+++ code/test_CNN.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for j in {0..4}; do
-    echo "start iterations for the $j loss function"
+# for j in {0..4}; do
+#     echo "start iterations for the $j loss function"
     for i in {1..2}; do
         echo "Running iteration $i"
-        nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 2 -l $j
-        nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 4 -l $j
-        nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 2 -l $j
-        nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 4 -l $j
-        nice -n 5 python3.11 cnn_eq_training_sym.py -m QAM -o 4 -l $j
+        nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 2 -l 5
+        nice -n 5 python3.11 cnn_eq_training_sym.py -m ASK -o 4 -l 5
+        nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 2 -l 1
+        nice -n 5 python3.11 cnn_eq_training_sym.py -m PAM -o 4 -l 1
+        nice -n 5 python3.11 cnn_eq_training_sym.py -m QAM -o 4 -l 0
     done
-    echo "All iterations for the $j loss function completed."
-done
+#     echo "All iterations for the $j loss function completed."
+# done
 echo "ALL DONE"
