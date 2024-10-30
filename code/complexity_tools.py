527c4361aeca66a5102f7acb9edbe87d1c1024f8
Diego Figueroa <diego.castillo@student.kit.edu>
2024-10-30 11:00:00 +0100

    design_conv_layer working good

--- code/complexity_tools.py
+++ code/complexity_tools.py
@@ -34,22 +34,44 @@ def design_CNN_structures(complexity: int, complexity_profile: np.ndarray, CNN_c
 def design_conv_layer(complexity: float, structure: np.ndarray, layer_idx: int, num_new_structures: int, CNN_ch_out: int):
     layer_ch_in = structure[0,layer_idx]
     strides = structure[3,:]
-    groups = structure[4, layer_idx]
-    prod_ch_out_ker_sz = complexity*groups*CNN_ch_out/(layer_ch_in*np.prod(strides[layer_idx+1:]))
-    ch_out_options = np.logspace((1/(num_new_structures+1)), np.log10(prod_ch_out_ker_sz), num_new_structures, endpoint=False)
-    k_size_options = prod_ch_out_ker_sz/ch_out_options
+    groups_current = structure[4, layer_idx]
+    # if we are in the last layer:
+    if layer_idx+1 == strides.size:
+        structure[1,layer_idx] = round_ch_out(CNN_ch_out, groups_current)
+        structure[2,layer_idx] = complexity*groups_current/(layer_ch_in*np.prod(strides[layer_idx+1:]))
+        return [structure,]
+    groups_next = structure[4, layer_idx+1]
+    prod_layer_ch_out_ker_sz = complexity*groups_current*CNN_ch_out/(layer_ch_in*np.prod(strides[layer_idx+1:]))
+    layer_ch_out_options = np.logspace((1/(num_new_structures+1)), np.log10(prod_layer_ch_out_ker_sz), num_new_structures, endpoint=False)
+    k_size_options = prod_layer_ch_out_ker_sz/layer_ch_out_options
     structures = []
-    for ch_out, k_size in zip(ch_out_options, k_size_options):
+    for layer_ch_out, k_size in zip(layer_ch_out_options, k_size_options):
         new_structure = structure.copy()
-        new_structure[1,layer_idx] = ch_out
-        new_structure[2,layer_idx] = k_size
-        new_structure[0,layer_idx+1] = ch_out       # input of the next layer is output of the current one
+        new_structure[1,layer_idx] = round_ch_out(layer_ch_out, groups_current, groups_next)
+        new_structure[2,layer_idx] = round_kernel_size(k_size)
+        new_structure[0,layer_idx+1] = round_ch_out(layer_ch_out, groups_current, groups_next)       # input of the next layer is output of the current one
         structures.append(new_structure)
     return structures
 
+def round_ch_out(ch_out: float, groups_pre: int=1, groups_post: int=1):
+    ''' Returns the smallest integer divisible by groups_current_pre and groups_current_post bigger than ch_out
+    '''
+    divisible_by = np.lcm(groups_pre, groups_post)
+    remainder = np.mod(np.ceil(ch_out), divisible_by)
+    if remainder == 0:
+        return int(np.ceil(ch_out))
+    return int(np.ceil(ch_out)+divisible_by-remainder)
+
+def round_kernel_size(kernel_size: float):
+    ''' Returns the smallest odd number bigger than kernel_size
+    '''
+    return int(np.ceil(kernel_size) + np.mod(np.ceil(kernel_size)+1,2))
 
 if __name__=="__main__":
-    xd = design_conv_layer(500, np.array([[1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[1,1,1,1],[1,1,1,1]]),0,5)
-    xd = design_conv_layer(500,xd[1],1,5)
-    xd = design_conv_layer(500,xd[1],2,5)
-    print(len(xd))
+    xd = design_conv_layer(500, np.array([[1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[1,1,1,1],[1,3,5,1]]),0,5,1)
+    xd = design_conv_layer(500,xd[1],1,5,1)
+    xd = design_conv_layer(500,xd[1],2,5,1)
+    # xd = design_conv_layer(500,xd[1],3,5,1)
+    
+    for xxd in xd:
+        print(xxd)
