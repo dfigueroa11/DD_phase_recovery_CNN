8dd934773070158c9be07b82823e758934351bc7
Diego Figueroa <diego.castillo@student.kit.edu>
2024-11-05 11:30:44 +0100

    parallelize programs 2 by 2

--- code/test_CNN.sh
+++ code/test_CNN.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# for j in {0..4}; do
-#     echo "start iterations for the $j loss function"
-    for i in {1..1}; do
-        echo "Running iteration $i"
-        nice -n 5 python3.11 cnn_eq_training_complexity.py -m ASK -o 2 -l 5
-        nice -n 5 python3.11 cnn_eq_training_complexity.py -m ASK -o 4 -l 5
-        nice -n 5 python3.11 cnn_eq_training_complexity.py -m PAM -o 2 -l 1
-        nice -n 5 python3.11 cnn_eq_training_complexity.py -m PAM -o 4 -l 1
-        nice -n 5 python3.11 cnn_eq_training_complexity.py -m QAM -o 4 -l 0
-    done
-#     echo "All iterations for the $j loss function completed."
-# done
+nice -n 5 python3.11 cnn_eq_training_complexity.py -m ASK -o 2 -l 5 &
+nice -n 5 python3.11 cnn_eq_training_complexity.py -m ASK -o 4 -l 5 &
+wait
+nice -n 5 python3.11 cnn_eq_training_complexity.py -m PAM -o 2 -l 1 &
+nice -n 5 python3.11 cnn_eq_training_complexity.py -m PAM -o 4 -l 1 &
+wait
+nice -n 5 python3.11 cnn_eq_training_complexity.py -m QAM -o 4 -l 0 &
+wait
 echo "ALL DONE"
