d6997718ea06457d66cdeb6db717a9fa57ad9e72
Diego Figueroa <diego.castillo@student.kit.edu>
2024-11-25 14:05:19 +0100

    new parallel test

--- code/test_CNN.sh
+++ code/test_CNN.sh
@@ -2,8 +2,9 @@
 
 for i in {0,1,2}
 do
-    python3.11 fcn_eq_training.py -m ASK -o 2 -l $i
-    python3.11 fcn_eq_training.py -m ASK -o 4 -l $i
-    python3.11 fcn_eq_training.py -m QAM -o 4 -l $i
+    python3.11 fcn_eq_training.py -m ASK -o 2 -l $i &
+    python3.11 fcn_eq_training.py -m ASK -o 4 -l $i &
+    python3.11 fcn_eq_training.py -m QAM -o 4 -l $i &
+    wait
 done
 echo "ALL DONE"
