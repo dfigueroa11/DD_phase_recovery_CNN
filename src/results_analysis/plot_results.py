f3087a0401ef58c8c1859945631c0e3091e72a97
Diego Figueroa <diego.castillo@student.kit.edu>
2025-01-08 16:22:14 +0100

    basic ploting done

--- src/results_analysis/plot_results.py
+++ src/results_analysis/plot_results.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from cycler import cycler
 
 def read_file_data(file_path: str, delimiter: str=','):
     with open(file_path, 'r') as file:
@@ -37,22 +41,30 @@ def take_x_y_slice(x_data, y_data, slice_data, slice_values):
         y_data_out.append(y[np.all(s == s_val,-1)])
     return x_data_out, y_data_out
 
-def plot_data(file_paths, x_data_selection, y_data_selection, slice_selection, slice_values):
+def plot_data(ax: Axes, file_paths: list[str], x_data_selection: list[list[str]], y_data_selection: list[list[str]],
+              slice_selection: list[list[str]], slice_values: list[list[float]], labels_list: list[list[str]]):
     headers, data = read_data(file_paths)
     x_data, y_data, slice_data = select_data(headers, data, x_data_selection, y_data_selection, slice_selection)
     x_data, y_data = take_x_y_slice(x_data, y_data, slice_data, slice_values)
-
-    return 0 
+    for x, y, lbl in zip(x_data, y_data, labels_list):
+        ax.plot(x, y, label=lbl)
 
 
 
 
 mod_formats = ["ASK2", "ASK4", "PAM2", "PAM4", "QAM4"]
 file_paths = [f"/Users/diegofigueroa/Desktop/tvrnn_norm_results/{mod_format}.txt" for mod_format in mod_formats]
-x_data_selection = [["MI", "SER"] for mod_format in mod_formats]
-y_data_selection = [["L_link_km"] for mod_format in mod_formats]
-slice_selection = [["SNR","L_link_km"] for mod_format in mod_formats]
-slice_values = [[1,6] for mod_format in mod_formats]
+y_data_selection = [["MI", "SER"] for mod_format in mod_formats]
+x_data_selection = [["SNR"] for mod_format in mod_formats]
+slice_selection = [["L_link_km"] for mod_format in mod_formats]
+slice_values = [6 for mod_format in mod_formats]
+labels_list = [[f"{mod_format} MI", f"{mod_format} SER"] for mod_format in mod_formats]
 
+# use the cycler to set the properties of each graph
+color_cycler = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+mpl.rcParams['axes.prop_cycle'] = cycler(color=color_cycler, ls=['--',]*len(color_cycler))
 
-plot_data(file_paths, x_data_selection, y_data_selection, slice_selection, slice_values)
+fig, ax = plt.subplots()
+plot_data(ax,file_paths, x_data_selection, y_data_selection, slice_selection, slice_values, labels_list)
+ax.legend()
+plt.show()
