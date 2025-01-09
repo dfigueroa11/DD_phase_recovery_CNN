3684b06e272d346b1f9c31d345605e5dfb2dd232
Diego Figueroa <diego.castillo@student.kit.edu>
2025-01-09 12:01:18 +0100

    leave only functions here

--- src/results_analysis/ploting_tools.py
+++ src/results_analysis/ploting_tools.py
@@ -86,23 +86,3 @@ def plot_data(ax: Axes, file_paths: list[str], x_data_selection: list[str], y_da
         x_data, y_data = take_x_y_slice(x_data, y_data, slice_data, slice_values)
     for x, y, lbl in zip(x_data, y_data, labels):
         ax.plot(x, y, label=lbl)
-
-
-
-
-mod_formats = ["ASK2", "ASK4", "PAM2", "PAM4", "QAM4"]
-file_paths = [f"/Users/diegofigueroa/Desktop/tvrnn_norm_results/{mod_format}.txt" for mod_format in mod_formats]
-y_data_selection = [["MI", "SER"] for mod_format in mod_formats]
-x_data_selection = ["SNR" for mod_format in mod_formats]
-slice_selection = [["L_link_km"] for mod_format in mod_formats]
-slice_values = [6 for mod_format in mod_formats]
-labels = [[f"{mod_format} MI", f"{mod_format} SER"] for mod_format in mod_formats]
-
-# use the cycler to set the properties of each graph
-color_cycler = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-mpl.rcParams['axes.prop_cycle'] = cycler(color=color_cycler, ls=['--',]*len(color_cycler))
-
-fig, ax = plt.subplots()
-plot_data(ax,file_paths, x_data_selection, y_data_selection, labels, slice_selection, slice_values)
-ax.legend()
-plt.show()
