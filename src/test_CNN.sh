ebfa7408934b3ebee9c1cb6c4a77d38d624b7c4f
Diego Figueroa <diego.castillo@student.kit.edu>
2025-02-10 15:41:57 +0100

    test different sizes of tvrnn

--- src/test_CNN.sh
+++ src/test_CNN.sh
@@ -2,11 +2,11 @@
 
 for i in {1,2,3,4}
 do
-    python3.11 tvrnn_eq_training.py -m ASK -o 2 -S 4 -s $i &
-    python3.11 tvrnn_eq_training.py -m ASK -o 4 -S 4 -s $i &
-    python3.11 tvrnn_eq_training.py -m PAM -o 2 -S 4 -s $i &
-    python3.11 tvrnn_eq_training.py -m PAM -o 4 -S 4 -s $i &
-    python3.11 tvrnn_eq_training.py -m QAM -o 4 -S 4 -s $i &
+    python3.11 tvrnn_eq_training.py -m ASK -o 2
+    python3.11 tvrnn_eq_training.py -m ASK -o 4
+    python3.11 tvrnn_eq_training.py -m PAM -o 2
+    python3.11 tvrnn_eq_training.py -m PAM -o 4
+    python3.11 tvrnn_eq_training.py -m QAM -o 4
     wait
 done
 echo "ALL DONE"
--- src/tvrnn_eq_training.py
+++ src/tvrnn_eq_training.py
@@ -78,7 +78,7 @@ def eval_n_save_rnn():
     MI = MI/num_frame_validation
     SERs = SERs/num_frame_validation
 
-    io_tool.print_save_summary(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}.txt", lr, L_link, alpha, SNR_dB, SERs, MI)
+    io_tool.print_save_summary(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}_L_y={L_y}_hs={hidden_states_size}.txt", lr, L_link, alpha, SNR_dB, SERs, MI)
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -97,8 +97,8 @@ N_taps = 41
 R_sym = 35e9
 beta2 = -2.168e-26
 alpha = 0
-L_link_steps = np.arange(0,35,6)*1e3      # for sweep over L_link
-L_link_save_fig = L_link_steps[[0,2,-1]]
+L_link_steps = np.array([0,12,30])*1e3      # for sweep over L_link
+L_link_save_fig = L_link_steps
 SNR_dB_steps = np.arange(-5, 12, 2)                          # for sweep over SNR
 SNR_save_fig = SNR_dB_steps[[0,5,-2,-1]]
 train_type = list(rnn.TRAIN_TYPES.keys())[args.loss_func]
@@ -110,11 +110,11 @@ curr_stage = args.currentStage
 known_stages = curr_stage-1
 unknown_stages = num_SIC_stages - known_stages
 ### TVRNN definition
-L_y = 32
+L_y_list = [10, 20, 30]
 L_ic = 16
 eff_L_ic = L_ic*2 if complex_mod else L_ic
-input_size = L_y if curr_stage == 1 else L_y + eff_L_ic
-hidden_states_size = np.array([32,])
+input_size_list = [L_y if curr_stage == 1 else L_y + eff_L_ic for L_y in L_y_list] 
+hidden_states_size_list = [np.array([10,]), np.array([20,]), np.array([30,])]
 output_size = M
 
 ## NN Training parameters
@@ -128,28 +128,31 @@ num_frame_validation = 100
 num_epochs_before_sched = 100
 num_frame_sched_validation = 1
 
-## matrices to take the inputs 
-idx_mat_y_inputs, idx_mat_x_inputs = data_conv_tools.gen_idx_mat_inputs(n_sym_per_batch*batch_size, N_os, L_y, L_ic, num_SIC_stages, curr_stage)
-# reshape and use index of the SIC block acording to the paper: (s,t)
-idx_mat_y_inputs = idx_mat_y_inputs.reshape(batch_size, t_max, unknown_stages, -1).permute(0,2,1,3).to(device)
-idx_mat_x_inputs = idx_mat_x_inputs.reshape(batch_size, t_max, unknown_stages, -1).permute(0,2,1,3).to(device)
-
-## saving routine
-save_progress = True
 
 folder_path = io_tool.create_folder(f"results2/{train_type_name}/{mod_format}{M:}",0)
-io_tool.init_summary_file(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}.txt")
 
-for L_link in L_link_steps:
-    for SNR_dB in SNR_dB_steps:
-        print(f'training model with L_link={L_link*1e-3:.0f}km, SNR={SNR_dB} dB, for {mod_format}-{M}, stage: {curr_stage}/{num_SIC_stages}')
-        dd_system = initialize_dd_system()
-        _, _, _, y = dd_system.simulate_transmission(1, int(100e3), SNR_dB)
-        y_mean, y_var, x_mean, x_var = hlp.find_normalization_constants(y, dd_system.constellation, SNR_dB)
-        rnn_eq, optimizer, scheduler = initialize_RNN_optimizer(lr)
-        if save_progress:
-            progress_file_path = f"{folder_path}/progress_S={num_SIC_stages}_s={curr_stage}_{io_tool.make_file_name(lr, L_link, alpha, SNR_dB)}.txt"
-            io_tool.init_progress_file(progress_file_path)
-        train_rnn()
-        eval_n_save_rnn()
-io_tool.write_complexity_in_summary_file(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}.txt", rnn_eq.complexity)
+for input_size, L_y in zip(input_size_list, L_y_list):
+    for hidden_states_size in hidden_states_size_list:
+        ## matrices to take the inputs 
+        idx_mat_y_inputs, idx_mat_x_inputs = data_conv_tools.gen_idx_mat_inputs(n_sym_per_batch*batch_size, N_os, L_y, L_ic, num_SIC_stages, curr_stage)
+        # reshape and use index of the SIC block acording to the paper: (s,t)
+        idx_mat_y_inputs = idx_mat_y_inputs.reshape(batch_size, t_max, unknown_stages, -1).permute(0,2,1,3).to(device)
+        idx_mat_x_inputs = idx_mat_x_inputs.reshape(batch_size, t_max, unknown_stages, -1).permute(0,2,1,3).to(device)
+
+        ## saving routine
+        save_progress = True
+
+        io_tool.init_summary_file(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}_L_y={L_y}_hs={hidden_states_size}.txt")
+        for L_link in L_link_steps:
+            for SNR_dB in SNR_dB_steps:
+                print(f'training model with L_link={L_link*1e-3:.0f}km, SNR={SNR_dB} dB, for {mod_format}-{M} L_y={L_y}, hs={hidden_states_size}')
+                dd_system = initialize_dd_system()
+                _, _, _, y = dd_system.simulate_transmission(1, int(100e3), SNR_dB)
+                y_mean, y_var, x_mean, x_var = hlp.find_normalization_constants(y, dd_system.constellation, SNR_dB)
+                rnn_eq, optimizer, scheduler = initialize_RNN_optimizer(lr)
+                if save_progress:
+                    progress_file_path = f"{folder_path}/progress_S={num_SIC_stages}_s={curr_stage}_L_y={L_y}_hs={hidden_states_size}_{io_tool.make_file_name(lr, L_link, alpha, SNR_dB)}.txt"
+                    io_tool.init_progress_file(progress_file_path)
+                train_rnn()
+                eval_n_save_rnn()
+        io_tool.write_complexity_in_summary_file(f"{folder_path}/results_S={num_SIC_stages}_s={curr_stage}_L_y={L_y}_hs={hidden_states_size}.txt", rnn_eq.complexity)
