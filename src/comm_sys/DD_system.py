fix constellation normalization

src/comm_sys/DD_system.py
@@ -28,7 +28,7 @@ class DD_system():
         self.N_os = N_os
         self.N_sim = N_sim
         self.d = N_sim//N_os
-        self.constellation = (constellation/torch.sqrt(constellation.var(correction=0))).to(device)
+        self.constellation = (constellation/torch.sqrt(constellation.abs().square().mean())).to(device)
         self.phase_list = torch.torch.unique(torch.round(torch.angle(constellation), decimals=10)).to(device)
         self.phase_list,_ = torch.sort(torch.remainder(self.phase_list,2*torch.pi))
         self.multi_mag_const = torch.unique(torch.round(torch.abs(self.constellation), decimals=5)).numel() > 1